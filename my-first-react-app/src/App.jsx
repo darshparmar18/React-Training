552af5de9d0c9cead0956c588b889062a2115910
darshparmar18 <darsh.p@sarvadhi.com>
2025-09-15 09:50:30 +0530

    rtyui

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -3,20 +3,34 @@ import ProfileCard from "./ProfileCard";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "30px" }}>
-      <h1>Props, State & Events</h1>
-      <ProfileCard name="Darsh Parmar" 
-      role="Node Developer"
-      age = {25} 
-      description = "I am a Node Developer" 
-      />
-
-      <ProfileCard name="Rahul Shah" 
-      role="React Developer" 
-      age = {30} 
-      description = "I am a React Developer" 
-      />
-    </div>
+    <>
+      <div style={{ textAlign: "center", marginBottom: "10px" }}>
+        <h1>Props, State & Events</h1>
+      </div>
+      <div style={{ 
+        textAlign: "center", 
+        marginTop: "30px", 
+        display: "flex",
+        justifyContent: "center",
+        gap: "20px"
+      }}> 
+        <ProfileCard name="Darsh Parmar" 
+        role="Node Developer"
+        age = {25} 
+        description = "I am a Node Developer" 
+        />
+        <ProfileCard name="Rahul Shah" 
+        role="React Developer" 
+        age = {30} 
+        description = "I am a React Developer" 
+        />
+        <ProfileCard name="Priya Jain" 
+        role="UI/UX Designer" 
+        age = {20} 
+        description = "I am a UI/UX Designer" 
+        />
+      </div>
+    </>
   );
 }
 
